d7c0d858569d59d82eb51d8b5c77c59aad5b16d2
Thanasak <Thanasaksrisaeng@gmail.com>
2021-04-27 19:28:07 +0700

    add Procfile, update port and remove node_modules

--- app.js
+++ app.js
@@ -135,6 +135,12 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log("Server has started successfully");
 });
